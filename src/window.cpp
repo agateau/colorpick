f3d9a08b00f6805bd86b5260d4d2da7faf115c55
Aurélien Gâteau <mail@agateau.com>
2016-09-02 07:58:43 +0200

    Move preview handling to a separate widget

--- src/window.cpp
+++ src/window.cpp
@@ -1,16 +1,9 @@
 #include "window.h"
 
 #include "coloreditor.h"
+#include "contrastpreview.h"
 
-#include <KColorUtils>
-
-#include <QLabel>
-#include <QLocale>
 #include <QVBoxLayout>
-
-// As per http://www.w3.org/TR/WCAG20/#visual-audio-contrast
-static constexpr qreal ACCEPTABLE_CONTRAST_RATIO = 3;
-static constexpr qreal GOOD_CONTRAST_RATIO = 4.5;
 
 Window::Window(QWidget *parent) : QMainWindow(parent)
 {
@@ -28,51 +21,17 @@ Window::Window(QWidget *parent) : QMainWindow(parent)
     pal.setColor(QPalette::Window, pal.color(QPalette::Window).dark(110));
     mFgEditor->setPalette(pal);
 
-    mRatioLabel = new QLabel;
-    mDemoLabel = new QLabel(tr("Contrast Example Text"));
-    mDemoLabel->setAutoFillBackground(true);
-    mDemoLabel->setFrameStyle(QFrame::Box);
-    mDemoLabel->setMargin(6);
+    ContrastPreview *preview = new ContrastPreview;
 
-    connect(mBgEditor, &ColorEditor::colorChanged, this, &Window::onColorChanged);
-    connect(mFgEditor, &ColorEditor::colorChanged, this, &Window::onColorChanged);
+    connect(mBgEditor, &ColorEditor::colorChanged, preview, &ContrastPreview::setBackgroundColor);
+    connect(mFgEditor, &ColorEditor::colorChanged, preview, &ContrastPreview::setForegroundColor);
 
     layout->setContentsMargins(QMargins());
     layout->addWidget(mBgEditor);
     layout->addWidget(mFgEditor);
-    layout->addWidget(mRatioLabel);
-    layout->addWidget(mDemoLabel);
+    layout->addWidget(preview);
 
     setCentralWidget(centralWidget);
     mBgEditor->setColor(Qt::white);
     mFgEditor->setColor(Qt::black);
-}
-
-void Window::onColorChanged()
-{
-    updateRatioLabel();
-    updateDemoLabel();
-}
-
-void Window::updateRatioLabel()
-{
-    qreal ratio = KColorUtils::contrastRatio(mBgEditor->color(), mFgEditor->color());
-    QString level;
-    if (ratio < ACCEPTABLE_CONTRAST_RATIO) {
-        level = tr("Bad");
-    } else if (ratio < GOOD_CONTRAST_RATIO) {
-        level = tr("Acceptable");
-    } else {
-        level = tr("Good");
-    }
-    QString text = tr("Contrast Ratio: %1:1 (%2)").arg(QLocale::system().toString(ratio, 'g', 2)).arg(level);
-    mRatioLabel->setText(text);
-}
-
-void Window::updateDemoLabel()
-{
-    QPalette pal = QPalette();
-    pal.setColor(QPalette::Window, mBgEditor->color());
-    pal.setColor(QPalette::WindowText, mFgEditor->color());
-    mDemoLabel->setPalette(pal);
 }
