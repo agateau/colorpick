623074abd2b2fcb279db496ec1e7a03d5e650c9f
Aurelien Gateau <mail@agateau.com>
2020-10-20 09:05:01 +0200

    Add an app icon
    Fixes #1

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    Q_INIT_RESOURCE(app);
+    auto iconName = QString(":/icons/sc-apps-colorpick.svg");
+    app.setWindowIcon(QIcon(iconName));
     Window window;
     window.show();
     return app.exec();
